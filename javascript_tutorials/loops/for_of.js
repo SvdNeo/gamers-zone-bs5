583e9714595bc750f345c448abd65192d8029b8f
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-03 17:45:39 +0530

    for of array

---
+++ javascript_tutorials/loops/for_of.js
@@ -0,0 +1,11 @@
+let game2 = {
+    "name": "Rakshi",
+    "id": 2,
+    "age": 22,
+    "fav_games": ["fortnite", "call of duty", "pubg", "valorent"]
+}
+
+
+for (let game in game2.fav_games) {
+    console.log(game)
+}
