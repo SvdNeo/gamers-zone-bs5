d39380681d8fed5acaecf85bd586f47ed86d38df
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-03 17:42:45 +0530

    for in loop

---
+++ javascript_tutorials/loops/for_in.js
@@ -0,0 +1,14 @@
+let gamer = {
+    "name": "Sumanth",
+    "id": 1,
+    "age": 22,
+    "fav_games": ["fortnite", "call of duty", "pubg"]
+}
+
+for (let key_temp in gamer) {
+    console.log(key_temp)
+}
+
+for (let key in gamer) {
+    console.log(gamer[key])
+}
