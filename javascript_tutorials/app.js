6f55b17291a4c2891e5aa33c05be9de8cd6b5265
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-03 16:28:32 +0530

    basic for loop

--- javascript_tutorials/app.js
+++ javascript_tutorials/app.js
@@ -1,35 +1,35 @@
-alert("Hey there is IPL match in Bengaluru");
+// alert("Hey there is IPL match in Bengaluru");
 
 
-const name = prompt('Please enter your name:');
-console.log('Hello, ' + name); // Logs a message with the entered name
+// const name = prompt('Please enter your name:');
+// console.log('Hello, ' + name); // Logs a message with the entered name
 
 
-const result = confirm('Are you sure you want to learn full stack development?');
-console.log(result)
+// const result = confirm('Are you sure you want to learn full stack development?');
+// console.log(result)
 
 
 
-var age = 23;
-var place = "Bengaluru";
-const designation = null;
-var company;
-var voter = true;
+// var age = 23;
+// var place = "Bengaluru";
+// const designation = null;
+// var company;
+// var voter = true;
 
-const fruits = ["apple", "mango", "pineapple", "banana"];
-const student = {
-    name: "Deepak",
-    "age": 15,
-    company: "Intel",
-    designation: "Intern",
-    salary: 30000
-};
+// const fruits = ["apple", "mango", "pineapple", "banana"];
+// const student = {
+//     name: "Deepak",
+//     "age": 15,
+//     company: "Intel",
+//     designation: "Intern",
+//     salary: 30000
+// };
 
 
-function order() {
-    console.log("This is order")
-}
+// function order() {
+//     console.log("This is order")
+// }
 
-order();
+// order();
 
 
