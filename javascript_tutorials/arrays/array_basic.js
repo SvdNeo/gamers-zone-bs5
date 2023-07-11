7e93e4b86770e56fa6c3273a433067cb23e42dcd
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-11 12:56:44 +0530

    changes

--- javascript_tutorials/arrays/array_basic.js
+++ javascript_tutorials/arrays/array_basic.js
@@ -2,18 +2,18 @@ let myArray = [1, 2, 3, 4, 5];
 console.log(myArray);
 
 let fruits = ['apple', 'banana', 'orange'];
-fruits.push('grape');
+let res1 = fruits.push('grape');
 console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape']
 
-fruits.pop();
+let res2 = fruits.pop();
 console.log(fruits); // Output: ['apple', 'banana', 'orange']
 
-fruits.shift();
+let res3 = fruits.shift();
 console.log(fruits); // Output: ['banana', 'orange']
 
-fruits.unshift('kiwi');
+let res4 = fruits.unshift('kiwi');
 console.log(fruits); // Output: ['kiwi', 'banana', 'orange']
 
-fruits.splice(1, 1, 'cherry', 'mango');
+let res5 = fruits.splice(0, 2, 'cherry', 'mango');
 console.log(fruits); // Output: ['kiwi', 'cherry', 'mango', 'orange']
 
