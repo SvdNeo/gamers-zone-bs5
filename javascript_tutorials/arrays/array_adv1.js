b6ddc981847f874f9b655673c8a67759914ef3a0
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-11 17:29:53 +0530

    more examples

--- javascript_tutorials/arrays/array_adv1.js
+++ javascript_tutorials/arrays/array_adv1.js
@@ -1,27 +1,28 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Using map to double each number
-let doubledNumbers = numbers.map(function (number) {
-    return number * 2;
-});
-console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+// let doubledNumbers = numbers.map(function (number) {
+//     return number * 2;
+// });
+// console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
 
-// Using filter to get even numbers
-let evenNumbers = numbers.filter(function (number) {
-    return number % 2 === 0;
-});
-console.log(evenNumbers); // Output: [2, 4]
+// // Using filter to get even numbers
+// let evenNumbers = numbers.filter(function (number) {
+//     return number % 2 === 0;
+// });
+// console.log(evenNumbers); // Output: [2, 4]
 
 // Using reduce to sum all numbers
-let sum = numbers.reduce(function (accumulator, number) {
+//[1, 2, 3, 4, 5];
+let cb = (accumulator, number) => {
     return accumulator + number;
-}, 0);
+}
+let sum = numbers.reduce(cb);
 console.log(sum); // Output: 15
 
+let cb2 = (num) => num % 2 === 0;
 // Using find to get the first even number
-let firstEvenNumber = numbers.find(function (number) {
-    return number % 2 === 0;
-});
+let firstEvenNumber = numbers.find(cb2);
 console.log(firstEvenNumber); // Output: 2
 
 
--- javascript_tutorials/arrays/array_basic.js
+++ javascript_tutorials/arrays/array_basic.js
@@ -1,19 +1,29 @@
 let myArray = [1, 2, 3, 4, 5];
-console.log(myArray);
+// console.log(myArray);
 
 let fruits = ['apple', 'banana', 'orange'];
-let res1 = fruits.push('grape');
-console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape']
 
-let res2 = fruits.pop();
-console.log(fruits); // Output: ['apple', 'banana', 'orange']
+let res1 = fruits.push('grape', 'kiwi');
+// console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape','kiwi']
 
-let res3 = fruits.shift();
-console.log(fruits); // Output: ['banana', 'orange']
+let fruits2 = ['mango', 'butterfruit', [["Test"], ['Lychee']]];
+// fruits.push(fruits2)
+// fruits2.forEach((fruit) => fruits.push(fruit))
+// for (let i = 0; i < fruits2.length; i++) fruits.push(fruits2[i])
+// for (let item of fruits2) fruits.push(item)
 
-let res4 = fruits.unshift('kiwi');
-console.log(fruits); // Output: ['kiwi', 'banana', 'orange']
+// fruits.push(fruits2) // 'mango', 'butterfruit', 'Lychee'
+// console.log(fruits)
 
-let res5 = fruits.splice(0, 2, 'cherry', 'mango');
+// let res2 = fruits.pop();
+// console.log(fruits); // Output: ['apple', 'banana', 'orange','grape']
+
+// let res3 = fruits.shift();
+// console.log(fruits); // Output: ['banana', 'orange']
+
+// let res4 = fruits.unshift('kiwi');
+// console.log(fruits); // Output: ['kiwi', 'banana', 'orange']
+// ['apple', 'banana', 'orange', 'grape', 'kiwi']
+let res5 = fruits.splice(2, 2, fruits2);
 console.log(fruits); // Output: ['kiwi', 'cherry', 'mango', 'orange']
 
