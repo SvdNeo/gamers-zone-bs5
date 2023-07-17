db8a6fe782c7fe29fefc3e962d7fe767d9b8f899
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-17 18:27:05 +0530

    for of

---
+++ es6_concepts/for_of.js
@@ -0,0 +1,14 @@
+const cars = ["BMW", "Volvo", "Mini"];
+let text1 = "";
+
+for (let x of cars) {
+    text2 += x + " ";
+}
+
+let language = "JavaScript";
+let text2 = "";
+
+for (let x of language) {
+    text2 += x + " ";
+}
+
