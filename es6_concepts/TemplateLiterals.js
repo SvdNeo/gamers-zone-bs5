eb3e1b524f3d4897b57002cb7a44f1188b37e61c
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-17 17:01:00 +0530

    Template Literal

---
+++ es6_concepts/TemplateLiterals.js
@@ -0,0 +1,7 @@
+const first_name = "Hritik";
+const last_name = "Roshan";
+
+console.log('Hello ' + first_name + ' ' + last_name);
+
+// Template Literals
+console.log(`Hello ${first_name} ${last_name}`);
