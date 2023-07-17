5ea26d142ed71acf1115854e8755c554409137dd
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-17 18:29:12 +0530

    Maths

---
+++ es6_concepts/math.js
@@ -0,0 +1,16 @@
+let res = Math.trunc(4.9);    // returns 4
+console.log(res)
+Math.trunc(4.7);    // returns 4
+Math.trunc(4.4);    // returns 4
+Math.trunc(4.2);    // returns 4
+Math.trunc(-4.2);    // returns -4
+
+Math.sign(-4);    // returns -1
+Math.sign(0);    // returns 0
+Math.sign(4);    // returns 1
+
+Math.log2(2);    // returns 1
+
+Math.log10(10);    // returns 1
+
+console.log(Math.PI)
