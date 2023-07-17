3750ad3174d11b1f72c55e84b9824bb50d7abd6d
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-17 18:26:30 +0530

    big int

---
+++ es6_concepts/bigint.js
@@ -0,0 +1,11 @@
+// To create a BigInt, append n to the end of an integer or call BigInt():
+
+let a = 1234567890123456789012345n;
+let b = BigInt(1234567890123456789012345)
+
+let x = BigInt(999999999999999);
+let type = typeof x;
+let res = 303030;
+console.log(type)
+console.log(typeof res)
+
