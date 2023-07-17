beabe25f6bbb154a32df433937eb48c98fd7e51b
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-17 17:06:47 +0530

    Object Destructuring

---
+++ es6_concepts/destructuring.js
@@ -0,0 +1,20 @@
+// before you would do something like this
+const person = {
+    name: 'Riya',
+    age: 22,
+    gender: 'female'
+}
+
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+
+console.log(name); // Riya
+console.log(age); // 22
+console.log(gender); // female
+
+let { name: name_1, age: age_1, gender: gender_1 } = person;
+
+console.log(name_1); // Riya
+console.log(age_1); // 22
+console.log(gender_1); // female
