880e9724cdd0ec7eb146c5e7378ca0b639064086
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-19 03:06:20 +0530

    css class related

---
+++ dom/class_related.js
@@ -0,0 +1,50 @@
+// Adding CSS using JavaScript:
+// Selecting an element by ID and modifying its CSS property
+document.getElementById('myElement').style.color = 'red';
+
+// Manipulating classes using classList:
+// Adding a class to an element
+document.getElementById('myElement').classList.add('highlight');
+
+// Removing a class from an element
+document.getElementById('myElement').classList.remove('highlight');
+
+// Adding a class to an element using className property
+document.getElementById('myElement').className += ' highlight';
+
+// Modifying the innerHTML of an element
+document.getElementById('myElement').innerHTML = '<p>This is the updated content.</p>';
+
+// Using document.write() to write content directly to the document
+document.write('<h1>Welcome to my website!</h1>');
+
+// Using innerHTML to modify the content of an element
+document.getElementById('myElement').innerHTML = '<h1>Welcome to my website!</h1>';
+
+// Changing the background color of an element to red
+document.getElementById('myElement').style.backgroundColor = 'red';
+
+// Setting the font size and color of an element
+document.getElementById('myElement').style.fontSize = '18px';
+document.getElementById('myElement').style.color = 'blue';
+
+// Applying a box shadow to an element
+document.getElementById('myElement').style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.2)';
+
+// Adjusting border properties of an element
+document.getElementById('myElement').style.border = '2px solid red';
+document.getElementById('myElement').style.borderRadius = '10px';
+
+// Modifying text alignment and padding of an element
+document.getElementById('myElement').style.textAlign = 'center';
+document.getElementById('myElement').style.padding = '10px';
+
+// Applying scale and rotation to an element
+document.getElementById('myElement').style.transform = 'scale(1.2) rotate(45deg)';
+
+// Hiding an element
+document.getElementById('myElement').style.display = 'none';
+
+// Showing a hidden element
+document.getElementById('myElement').style.display = 'block';
+
