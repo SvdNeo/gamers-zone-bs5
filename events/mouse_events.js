50f9cc33a9dd6bf69b0dd485261effaee20075a3
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-19 16:46:50 +0530

    img id

--- events/mouse_events.js
+++ events/mouse_events.js
@@ -1,34 +1,35 @@
-const gameThumbnail = document.getElementById('gameThumbnail');
+// const gameThumbnail = document.getElementById('gameThumbnail_img');
 
-gameThumbnail.addEventListener('click', function () {
-    // Open game details or perform an action
-    console.log('Game thumbnail clicked!');
-});
-
-
-const gameCard1 = document.getElementById('gameCard');
-
-gameCard1.addEventListener('mouseover', function () {
-    // Change background color or apply hover effect
-    gameCard1.style.backgroundColor = 'lightblue';
-});
-
-const gameCard2 = document.getElementById('gameCard');
-
-gameCard2.addEventListener('mouseout', function () {
-    // Restore original background color or remove hover effect
-    gameCard2.style.backgroundColor = '';
-});
+// gameThumbnail.addEventListener('click', function () {
+//     // Open game details or perform an action
+//     console.log('Game thumbnail clicked!');
+//     gameThumbnail.src = "./player_icon.jpg"
+// });
 
 
-const draggableElement = document.getElementById('draggableElement');
+// const gameCard1 = document.getElementById('gameCard');
 
-draggableElement.addEventListener('mousedown', function () {
-    // Perform actions for drag and drop
-    console.log('Mouse button pressed on draggable element');
-});
+// gameCard1.addEventListener('mouseover', function () {
+//     // Change background color or apply hover effect
+//     gameCard1.style.backgroundColor = 'lightblue';
+// });
 
-// Add your JavaScript code here for the context menu logic
+// const gameCard2 = document.getElementById('gameCard');
+
+// gameCard2.addEventListener('mouseout', function () {
+//     // Restore original background color or remove hover effect
+//     gameCard2.style.backgroundColor = '';
+// });
+
+
+// const draggableElement = document.getElementById('draggableElement');
+
+// draggableElement.addEventListener('mousedown', function () {
+//     // Perform actions for drag and drop
+//     console.log('Mouse button pressed on draggable element');
+// });
+
+// // JavaScript code for the context menu logic
 const gameElement = document.getElementById('gameElement');
 const contextMenu = document.getElementById('contextMenu');
 
@@ -54,7 +55,7 @@ function hideContextMenu() {
     contextMenu.style.display = 'none';
 }
 
-// JavaScript code for the mouse events
+// // JavaScript code for the mouse events
 const gameCards = document.querySelectorAll('.game-card');
 
 gameCards.forEach(function (card) {
@@ -96,72 +97,3 @@ gameCards.forEach(function (card) {
     });
 });
 
-
-// JavaScript code for the keyboard events
-const searchInput = document.getElementById('searchInput');
-
-searchInput.addEventListener('keydown', function (event) {
-    console.log('Search input key down:', event.key);
-});
-
-searchInput.addEventListener('keyup', function (event) {
-    console.log('Search input key up:', event.key);
-});
-
-searchInput.addEventListener('keypress', function (event) {
-    console.log('Search input key press:', event.key);
-});
-
-// JavaScript code for the form events
-const addCardForm = document.getElementById('addCardForm');
-
-addCardForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    console.log('Form submitted');
-
-
-});
-
-
-const titleInput = document.getElementById('titleInput');
-const descriptionInput = document.getElementById('descriptionInput');
-
-titleInput.addEventListener('input', function (event) {
-    console.log('Title input changed:', this.value);
-});
-
-descriptionInput.addEventListener('input', function (event) {
-    console.log('Description input changed:', this.value);
-});
-
-titleInput.addEventListener('change', function (event) {
-    console.log('Title input changed and lost focus:', this.value);
-});
-
-descriptionInput.addEventListener('change', function (event) {
-    console.log('Description input changed and lost focus:', this.value);
-});
-
-titleInput.addEventListener('focus', function (event) {
-    console.log('Title input focused');
-});
-
-descriptionInput.addEventListener('focus', function (event) {
-    console.log('Description input focused');
-});
-
-titleInput.addEventListener('blur', function (event) {
-    console.log('Title input blurred');
-});
-
-descriptionInput.addEventListener('blur', function (event) {
-    console.log('Description input blurred');
-});
-
-titleInput.addEventListener('select', function (event) {
-    console.log('Title input text selected:', this.value);
-});
-
-descriptionInput.addEventListener('select', function (event) {
-    console.log('Description input text selected:', this.value);
-});
