b99747f1d13f85955d79a2f9b35fd1649273bac8
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-19 14:01:10 +0530

    Window or Page events

---
+++ events/window_or_page_events.js
@@ -0,0 +1,28 @@
+// JavaScript code for the window events
+window.addEventListener('load', function () {
+    console.log('Page finished loading');
+});
+
+window.addEventListener('resize', function () {
+    console.log('Window resized');
+});
+
+window.addEventListener('scroll', function () {
+    console.log('Scrolling occurred');
+});
+
+window.addEventListener('unload', function () {
+    console.log('Page unloaded');
+});
+
+window.addEventListener('orientationchange', function () {
+    console.log('Device orientation changed');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded event triggered');
+});
+
+document.addEventListener('readystatechange', function () {
+    console.log('Ready state changed:', document.readyState);
+});
