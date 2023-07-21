a9ef3f1f6c1227a2a64552d0d6c5e3ab2b096e2b
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-21 17:51:31 +0530

    Two sum

---
+++ js_programs/array/two_sum.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+
+// Example usage:
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target)); // Output: [0, 1] (nums[0] + nums[1] = 2 + 7 = 9)
