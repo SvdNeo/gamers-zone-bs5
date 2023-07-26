134bd1a8a08a90a4f3b7a5c881593269b2630fdc
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-26 18:03:22 +0530

    changes

--- json/json_test.js
+++ json/json_test.js
@@ -7,7 +7,9 @@ const parsedObject = JSON.parse(jsonString);
 console.log(parsedObject['name']); // "Harish Singh"
 console.log(parsedObject.hobbies[1]); // "swimming"
 console.log(parsedObject.address.city); // "Patiala"
-
+console.log(user["hobbies"])
+console.log(typeof user);
 person = JSON.stringify(user);
 
-console.log(JSON);
+console.log(person);
+console.log(typeof person);
