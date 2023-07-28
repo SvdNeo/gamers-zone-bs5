7d741422f787bf0b382f7a4ab2c0775062004fe8
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-28 18:25:17 +0530

    Sum of Array

--- advanced_js/recursion/SumOfArray.js
+++ advanced_js/recursion/SumOfArray.js
@@ -8,3 +8,10 @@ function sumArray(arr, index) {
 
 const numbers = [1, 2, 3, 4, 5];
 console.log(sumArray(numbers, 0)); // Output: 15 (1 + 2 + 3 + 4 + 5)
+
+//call stack s0()
+//execution a,i=0 1+14 ->15
+//a, i=1 2+12
+//a, i=2 3+9
+//a, i=3 4+5 
+//a i=4 5
