1b4401fe07dec03faa7a01b5ea9ac9172377cd60
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-28 17:11:38 +0530

    changes

--- advanced_js/recursion/Fibonacci.js
+++ advanced_js/recursion/Fibonacci.js
@@ -5,5 +5,22 @@ function fibonacci(n) {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
+let res = fibonacci(6);
+console.log(); // Output: 8 (0, 1, 1, 2, 3, 5, 8)
 
-console.log(fibonacci(6)); // Output: 8 (0, 1, 1, 2, 3, 5, 8)
+// n = 6
+//call stack = f6 
+/*
+execution f6 = 5 + 3
+f5 = 3 + 2
+f4 = 2 + 1 
+f3 = 1 +1
+f2 = 1 + 0
+
+
+
+
+
+
+
+*/
