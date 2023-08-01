050327fd0390cf21f95310056ccfa090b563ab07
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-01 17:00:13 +0530

    changes

--- advanced_js/this_in_detail/NormalFunction_and_GlobalContext.js
+++ advanced_js/this_in_detail/NormalFunction_and_GlobalContext.js
@@ -8,6 +8,10 @@ normalFunction(); // Output: [object Window] (in browsers)
 function strictFunction() {
     'use strict';
     console.log(this); // Refers to undefined in strict mode
+    function normalFunction() {
+        console.log(this); // Refers to the global object (e.g., window in browsers)
+    }
+    normalFunction()
 }
 
 strictFunction(); // Output: undefined
