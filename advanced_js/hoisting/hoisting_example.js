486c49354fb6d563a6f806aa239b23e4e840f52b
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-02 15:45:51 +0530

    Hoistign Example

---
+++ advanced_js/hoisting/hoisting_example.js
@@ -0,0 +1,30 @@
+// var college = "IIT Delhi";
+// console.log(college)
+
+// getMyBranch()
+// function getMyBranch() {
+
+//     var branch = "Computer Engineering"
+//     console.log(branch)
+// }
+
+
+// getMyBranch() // TypeError
+// var getMyBranch = function () {
+
+//     var branch = "Computer Engineering"
+//     console.log(branch)
+// }
+
+
+// getMyBranch() // ReferenceError 
+// let getMyBranch = function () {
+
+//     var branch = "Computer Engineering"
+//     console.log(branch)
+// }
+// let test;
+// console.log(test)
+
+console.log(favPlayer)
+const favPlayer = "M S D";
