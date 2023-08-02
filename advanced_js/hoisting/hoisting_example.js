b8b1928dd027acc207dc7a7a63ce3f31f3fda735
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-03 04:36:59 +0530

    Hoisting

--- advanced_js/hoisting/hoisting_example.js
+++ advanced_js/hoisting/hoisting_example.js
@@ -1,30 +1,30 @@
-// var college = "IIT Delhi";
-// console.log(college)
+var college = "IIT Delhi";
+console.log(college)
 
-// getMyBranch()
-// function getMyBranch() {
+getMyBranch()
+function getMyBranch() {
 
-//     var branch = "Computer Engineering"
-//     console.log(branch)
-// }
-
-
-// getMyBranch() // TypeError
-// var getMyBranch = function () {
-
-//     var branch = "Computer Engineering"
-//     console.log(branch)
-// }
+    var branch = "Computer Engineering"
+    console.log(branch)
+}
 
 
-// getMyBranch() // ReferenceError 
-// let getMyBranch = function () {
+getMyBranch() // TypeError
+var getMyBranch = function () {
 
-//     var branch = "Computer Engineering"
-//     console.log(branch)
-// }
-// let test;
-// console.log(test)
+    var branch = "Computer Engineering"
+    console.log(branch)
+}
+
+
+getMyBranch() // ReferenceError 
+let getMyBranch = function () {
+
+    var branch = "Computer Engineering"
+    console.log(branch)
+}
+let test;
+console.log(test)
 
 console.log(favPlayer)
 const favPlayer = "M S D";
